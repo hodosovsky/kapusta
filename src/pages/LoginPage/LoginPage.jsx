57e5a8e43fd0ecc7bb02fe6e923d614f12d84d2a
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2022-12-21 16:33:59 +0200

    revert first

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { LoginForm } from 'components/LoginForm/LoginForm';
 
-import { StyledLoginPage } from './LoginPage.styled';
-
 export default function LoginPage() {
   return (
-    <StyledLoginPage>
+    <div>
       <LoginForm />
-    </StyledLoginPage>
+    </div>
   );
 }
