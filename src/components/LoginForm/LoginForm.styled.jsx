b712c02791a7ec57b23e9ef17888bf95ecea9d57
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2022-12-21 15:44:55 +0200

    Revert "Styles login regist"

--- src/components/LoginForm/LoginForm.styled.jsx
+++ src/components/LoginForm/LoginForm.styled.jsx
@@ -1,30 +1,19 @@
 import styled from 'styled-components';
-import { NavLink } from 'react-router-dom';
 
 export const StyledformRegister = styled.div`
   position: absolute;
-  justify-content: center;
-  margin-left: auto;
-  margin-right: auto;
-  padding: 40px 20px 40px 20px;
-  top: 255px;
-  width: 240px;
+  left: 763px;
+  top: 173px;
+  transform: translate(-50%, 0);
   z-index: 1;
+  display: flex;
   flex-direction: column;
+  padding: 56px 84px 56px 84px;
+  width: 426px;
+  min-height: 552px;
   background-color: #fff;
   box-shadow: 0px 10px 60px rgba(170, 178, 197, 0.2);
   border-radius: 30px;
-
-  @media screen and (min-width: 768px) {
-    padding: 72px 84px 72px 84px;
-    top: 317px;
-    left: 171px;
-    width: 260px;
-  }
-  @media screen and (min-width: 1280px) {
-    left: 763px;
-    top: 173px;
-  }
 `;
 
 export const StyledpromtText = styled.p`
@@ -36,41 +25,13 @@ export const StyledpromtText = styled.p`
   text-align: center;
   letter-spacing: 0.04em;
   color: #52555f;
-  margin-bottom: 16px;
-
-  @media screen and (max-width: 767px) {
-    padding-left: 29px;
-    padding-right: 29px;
-  }
-  @media screen and (min-width: 768px) {
-    margin-bottom: 20px;
-  }
-`;
-
-export const StyledpromtText1 = styled.p`
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: 400;
-  font-size: 12px;
-  line-height: 1.16;
-  text-align: left;
-  letter-spacing: 0.04em;
-  color: #52555f;
-  margin-bottom: 16px;
-
-  @media screen and (min-width: 768px) {
-    margin-bottom: 20px;
-  }
+  margin-bottom: 20px;
 `;
 
 export const Styledlabel = styled.label`
   display: flex;
   flex-direction: column;
-  margin-bottom: 20px;
-
-  @media screen and (min-width: 768px) {
-    margin-bottom: 32px;
-  }
+  margin-bottom: 30px;
 `;
 
 export const StyledanimationGoogle = styled.div`
@@ -88,7 +49,10 @@ export const StyledLinkbtnGoogle = styled.a`
   background-color: #f6f7fb;
   border-radius: 26px;
   border: 0;
-  padding: 10px 18px 10px 20px;
+  width: 119px;
+  height: 40px;
+  padding-top: 10px;
+  padding-bottom: 10px;
   font-family: Roboto;
   font-style: normal;
   font-weight: 700;
@@ -105,23 +69,15 @@ export const StyledlabelText = styled.span`
   font-weight: 600;
   font-size: 10px;
   line-height: 12px;
-  color: #000000;
+  color: black;
 `;
 
 export const StyledformInput = styled.input`
-  display: flex;
-  justify-content: center;
   outline: none;
   border: none;
   border-radius: 30px;
   background-color: #f6f7fb;
-  padding: 17px 18px;
-  width: 204px;
-
-  @media screen and (min-width: 768px) {
-    padding: 17px 20px 17px 20px;
-    width: 218px;
-  }
+  padding: 20px 17px;
 `;
 
 export const StyledcontainerButton = styled.div`
@@ -129,8 +85,8 @@ export const StyledcontainerButton = styled.div`
   justify-content: space-between;
 `;
 
-export const StyledNavlink = styled(NavLink)`
-  width: 116px;
+export const StyledButton = styled.button`
+  width: 122px;
   height: 44px;
   background-color: #f5f6fb;
   border-radius: 16px;
@@ -140,8 +96,6 @@ export const StyledNavlink = styled(NavLink)`
   font-weight: 700;
   font-size: 12px;
   line-height: 1.16;
-  display: flex;
-  justify-content: center;
   align-items: center;
   text-align: center;
   letter-spacing: 0.02em;
@@ -149,18 +103,12 @@ export const StyledNavlink = styled(NavLink)`
   box-shadow: #52555f;
   filter: drop-shadow(1px 3px 5px rgba(82, 85, 95, 0.15));
   cursor: pointer;
-  align-content: center;
+
   color: #52555f;
   background-color: #f5f6fb;
 
-  text-decoration: none;
-  &.active {
+  :active {
     color: #ffffff;
     background-color: #ff751d;
-  }
-  :hover:not(.active),
-  :focus-visible:not(.active) {
-    color: #52555f;
-    background-color: #f5f6fb;
   }
 `;
