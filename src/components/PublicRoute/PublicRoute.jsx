cb982e5374059dd61eeeced60792f722210cf565
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2022-12-21 13:03:56 +0200

    add provider

---
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -0,0 +1,8 @@
+// import { useSelector } from 'react-redux';
+// import { Navigate, Outlet } from 'react-router-dom';
+// import authSelectors from 'redux/auth/auth-selectors';
+
+// export const PublicRoute = () => {
+//   const token = useSelector(authSelectors.selectToken);
+//   return token ? <Navigate to="/contacts" /> : <Outlet />;
+// };
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { store } from 'redux/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/kapusta">
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename="/kapusta">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
