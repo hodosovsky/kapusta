a70b9dfa19133128214c8f71c068549f445e528d
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2022-12-22 20:54:18 +0200

    add tablet styles

--- src/components/TransactionsList/TransactionsList.jsx
+++ src/components/TransactionsList/TransactionsList.jsx
@@ -1,19 +1,75 @@
-import deleteIcon from '../../images/deleteIcon.svg';
+import { ReactComponent as DeleteIcon } from '../../images/deleteIcon.svg';
+import {
+  ItemName,
+  ItemNameCont,
+  ItemDate,
+  ItemDateCont,
+  ItemStyled,
+  ItemCategoty,
+  Sum,
+  SumCont,
+  StyledList,
+} from './TransactionsList.styled';
 
 export const TransactionsList = () => {
-  console.log(deleteIcon);
   return (
-    <ul>
-      <li id={Date.now()}>
-        <div>
-          <p>Undeground</p>
-          <p>05.09.2019</p>
-        </div>
-        <p>Transport</p>
-        <p>- 30.00 UAH.</p>
+    <StyledList>
+      <ItemStyled key={Date.now() + 3}>
+        <ItemNameCont>
+          <ItemName>Description</ItemName>
+          <ItemDateCont>
+            <ItemDate>Date</ItemDate>
+            <ItemCategoty>category</ItemCategoty>
+          </ItemDateCont>
+        </ItemNameCont>
+        <SumCont>
+          <Sum className="sum">Sum</Sum>
 
-        <img width="18" height="18" src={deleteIcon} alt="delete" />
-      </li>
-    </ul>
+          <DeleteIcon id={Date.now()} />
+        </SumCont>
+      </ItemStyled>
+      <ItemStyled key={Date.now()}>
+        <ItemNameCont>
+          <ItemName>Undeground</ItemName>
+          <ItemDateCont>
+            <ItemDate>05.09.2019</ItemDate>
+            <ItemCategoty>Transport</ItemCategoty>
+          </ItemDateCont>
+        </ItemNameCont>
+        <SumCont>
+          <Sum className="sum">- 30.00 UAH.</Sum>
+
+          <DeleteIcon id={Date.now()} />
+        </SumCont>
+      </ItemStyled>
+      <ItemStyled key={Date.now() + 1}>
+        <ItemNameCont>
+          <ItemName>Bananas</ItemName>
+          <ItemDateCont>
+            <ItemDate>05.09.2019</ItemDate>
+            <ItemCategoty>Products</ItemCategoty>
+          </ItemDateCont>
+        </ItemNameCont>
+        <SumCont>
+          <Sum className="sum">- 50.00 UAH.</Sum>
+
+          <DeleteIcon id={Date.now()} />
+        </SumCont>
+      </ItemStyled>
+      <ItemStyled key={Date.now() + 2}>
+        <ItemNameCont>
+          <ItemName>My salary</ItemName>
+          <ItemDateCont>
+            <ItemDate>05.09.2019</ItemDate>
+            <ItemCategoty>ЗП</ItemCategoty>
+          </ItemDateCont>
+        </ItemNameCont>
+        <SumCont>
+          <Sum className="sum">20 000.00 UAH.</Sum>
+
+          <DeleteIcon id={Date.now()} />
+        </SumCont>
+      </ItemStyled>
+    </StyledList>
   );
 };
--- src/components/TransactionsList/TransactionsList.styled.jsx
+++ src/components/TransactionsList/TransactionsList.styled.jsx
@@ -1,3 +1,87 @@
 import styled from 'styled-components';
+export const StyledList = styled.ul`
+  & li:nth-of-type(1) {
+    font-weight: 700;
+    letter-spacing: 0.02em;
+  }
+`;
 
-export const StyledList = styled.ul``;
+export const ItemStyled = styled.li`
+  display: flex;
+  justify-content: space-between;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  border-bottom: 1px solid #f5f6fb;
+
+  @media screen and (min-width: 768px) {
+    justify-content: left;
+  }
+`;
+
+export const ItemName = styled.p`
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 1.16;
+  letter-spacing: 0.04em;
+  @media screen and (min-width: 768px) {
+    font-weight: 400;
+    width: 168px;
+    order: 1;
+  }
+`;
+
+export const ItemNameCont = styled.div`
+  @media screen and (min-width: 768px) {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export const ItemDateCont = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+export const ItemDate = styled.p`
+  font-size: 8px;
+  line-height: 1.12;
+  letter-spacing: 0.04em;
+  @media screen and (min-width: 768px) {
+    width: 84px;
+    order: 0;
+  }
+`;
+
+export const ItemCategoty = styled.p`
+  font-size: 8px;
+  line-height: 1.12;
+  letter-spacing: 0.04em;
+  @media screen and (min-width: 768px) {
+    width: 124px;
+  }
+`;
+export const SumCont = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 20px;
+
+  @media screen and (min-width: 768px) {
+    gap: 40px;
+  }
+`;
+export const Sum = styled.p`
+  display: flex;
+  align-items: center;
+
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 1.16;
+  letter-spacing: 0.04em;
+  vertical-align: auto;
+
+  @media screen and (min-width: 768px) {
+    width: 104px;
+    justify-content: flex-end;
+    text-transform: uppercase;
+  }
+`;
