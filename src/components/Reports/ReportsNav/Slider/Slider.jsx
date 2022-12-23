9d0e43d28d215555dbab56bad50d8e434fc6645d
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2022-12-23 20:22:50 +0200

    add reports to state

--- src/components/Reports/ReportsNav/Slider/Slider.jsx
+++ src/components/Reports/ReportsNav/Slider/Slider.jsx
@@ -1,30 +1,40 @@
-import {
-  SliderBox,
-  SliderText,
-} from './Slider.styled';
+import { SliderBox, SliderText } from './Slider.styled';
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { monthNames, getMonth, getYear } from './SliderUtils';
 import { Calendar } from './Calendar/Calendar';
 import { ButtonsNextPrev } from './ButtonsNextPrev/ButtonsNextPrev';
-
+import { getPeriodDataAPI } from 'services/apiTransactions';
 
 export const Slider = () => {
   const [monthNumber, setMonthNumber] = useState(0);
   const [month, setMonth] = useState('');
   const [year, setYear] = useState('');
-  const [modalCalendar, setModalCalendar]=useState(false)
+  const [modalCalendar, setModalCalendar] = useState(false);
+  const [reports, setReports] = useState([]);
 
   useEffect(() => {
     setMonthNumber(getMonth());
     setMonth(monthNames[getMonth()]);
     setYear(getYear());
   }, []);
+
   useEffect(() => {
     setMonth(monthNames[monthNumber]);
-  }, [monthNumber]);
+    let monthString = '';
 
-  const handlerClick = (name) => {
+    if (monthNumber + 1 < 10) {
+      monthString = '0' + (monthNumber + 1);
+    } else {
+      monthString = monthNumber + 1;
+    }
+
+    getPeriodDataAPI(`${year}-${monthString}`).then(data => {
+      setReports(data);
+    });
+  }, [monthNumber, year]);
+  console.log(reports);
+  const handlerClick = name => {
     switch (name) {
       case 'decrement':
         setMonthNumber(monthNumber - 1);
@@ -44,32 +54,41 @@ export const Slider = () => {
         return;
     }
   };
-  
-const handleModalCalendar = () =>{
-  setModalCalendar(modalCalendar=>!modalCalendar)
-}
 
-const handleCalendar = (name) => {
-  switch (name) {
-    case 'decrement':
+  const handleModalCalendar = () => {
+    setModalCalendar(modalCalendar => !modalCalendar);
+  };
+
+  const handleCalendar = name => {
+    switch (name) {
+      case 'decrement':
         setYear(year - 1);
-      break;
-    case 'increment':
+        break;
+      case 'increment':
         setYear(year + 1);
-      break;
-    default:
-      setMonthNumber(name)
-      return;
-  }
-}
+        break;
+      default:
+        setMonthNumber(name);
+        return;
+    }
+  };
 
   return (
     <SliderBox>
       <p>Current period:</p>
       <ButtonsNextPrev onButtonClick={handlerClick}>
-      <SliderText onClick={handleModalCalendar}>{month} {year}</SliderText>
+        <SliderText onClick={handleModalCalendar}>
+          {month} {year}
+        </SliderText>
       </ButtonsNextPrev>
-      {modalCalendar && <Calendar currentYear={year} currentMonth={month} onChangeDate={handleCalendar} onClose={handleModalCalendar}/>}
+      {modalCalendar && (
+        <Calendar
+          currentYear={year}
+          currentMonth={month}
+          onChangeDate={handleCalendar}
+          onClose={handleModalCalendar}
+        />
+      )}
     </SliderBox>
   );
 };
