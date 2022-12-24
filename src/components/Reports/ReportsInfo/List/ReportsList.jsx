c2100c44ac438e64ecb75174182bd488728ef026
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2022-12-24 20:42:00 +0200

    add incomes reports

--- src/components/Reports/ReportsInfo/List/ReportsList.jsx
+++ src/components/Reports/ReportsInfo/List/ReportsList.jsx
@@ -3,16 +3,18 @@ import { selectReports } from 'redux/selectors';
 import reportsIcon from '../../../../images/reportsFiles/reports.svg';
 import { List, Item, ItemSvg } from './ReportsList.styled';
 import { useState } from 'react';
+import { categoryOrkToEng } from 'hooks/useCategory';
 
 export const ReportsList = ({ onChange }) => {
   const [active, setActive] = useState('');
   const { reports } = useSelector(selectReports);
   const expensesData = reports?.expenses?.expensesData ?? {};
+  const incomesData = reports?.incomes?.incomesData ?? {};
   const clickEventHandler = event => {
     setActive(event.currentTarget.id);
   };
-  console.log(onChange);
   const entries = Object.entries(expensesData) ?? [];
+  const incomesEnties = Object.entries(incomesData) ?? [];
   return (
     <div>
       {onChange === 'expenses' && entries && (
@@ -31,28 +33,32 @@ export const ReportsList = ({ onChange }) => {
                 <ItemSvg width="56" height="56">
                   <use href={`${reportsIcon}#${iconName}`}></use>
                 </ItemSvg>
-                <p>{item[0]}</p>
+                <p>{categoryOrkToEng(item[0])}</p>
               </Item>
             );
           })}
         </List>
       )}
       {onChange === 'income' && (
-        <List className="incomeList">
-          <Item className="income" key={'male'}>
-            <p>{'item[1].total'}.00 male</p>
-            <ItemSvg width="56" height="56">
-              <use href={`${reportsIcon}#icon-salary`}></use>
-            </ItemSvg>
-            <p>{'item[0]'}</p>
-          </Item>
-          <Item className="income" key={'female'}>
-            <p>{'item[1].total'}.00 female</p>
-            <ItemSvg width="56" height="56">
-              <use href={`${reportsIcon}#icon-income`}></use>
-            </ItemSvg>
-            <p>{'item[0]'}</p>
-          </Item>
+        <List>
+          {incomesEnties.map(item => {
+            const iconName = item[0].replace(/\s+/g, '');
+
+            return (
+              <Item
+                key={iconName}
+                id={iconName}
+                onClick={clickEventHandler}
+                className={iconName === active ? 'active' : ''}
+              >
+                <p>{item[1].total}.00</p>
+                <ItemSvg width="56" height="56">
+                  <use href={`${reportsIcon}#${iconName}`}></use>
+                </ItemSvg>
+                <p>{categoryOrkToEng(item[0])}</p>
+              </Item>
+            );
+          })}
         </List>
       )}
     </div>
