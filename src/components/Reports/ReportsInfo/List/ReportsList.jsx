fdbc96be1a8a07bc076c9392ea0b364ca9601730
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2022-12-25 10:18:23 +0200

    add filtered category to redux state

--- src/components/Reports/ReportsInfo/List/ReportsList.jsx
+++ src/components/Reports/ReportsInfo/List/ReportsList.jsx
@@ -1,66 +1,74 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectReports } from 'redux/selectors';
 import reportsIcon from '../../../../images/reportsFiles/reports.svg';
 import { List, Item, ItemSvg } from './ReportsList.styled';
-import { useState } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { categoryOrkToEng } from 'hooks/useCategory';
+import { filteredDataAction } from 'redux/reportsQuery/reportsQuery.slice';
 
 export const ReportsList = ({ onChange }) => {
   const [active, setActive] = useState('');
   const { reports } = useSelector(selectReports);
-  const expensesData = reports?.expenses?.expensesData ?? {};
-  const incomesData = reports?.incomes?.incomesData ?? {};
+  const [data, setData] = useState({});
+  const dispatch = useDispatch();
+  const valueArr = [];
+  // const expensesData = reports?.expenses?.expensesData ?? {};
+
+  const expensesData = useMemo(
+    () => reports?.expenses?.expensesData ?? {},
+    [reports]
+  );
+
+  // const incomesData = reports?.incomes?.incomesData ?? {};
+  const incomesData = useMemo(
+    () => reports?.incomes?.incomesData ?? {},
+    [reports]
+  );
+
+  useEffect(() => {
+    if (onChange === 'expenses') {
+      setData(expensesData ?? {});
+    } else {
+      setData(incomesData ?? {});
+    }
+  }, [onChange, expensesData, incomesData]);
+
   const clickEventHandler = event => {
     setActive(event.currentTarget.id);
+    const filteredValueArr = valueArr.filter(
+      item => item[0].replace(/\s+/g, '') === event.currentTarget.id
+    );
+
+    dispatch(filteredDataAction(filteredValueArr));
   };
-  const entries = Object.entries(expensesData) ?? [];
-  const incomesEnties = Object.entries(incomesData) ?? [];
+  const entries = Object.entries(data) ?? [];
+  // const values = Object.values(data);
+
+  // setValueData(values);
+
+  // const incomesEnties = Object.entries(incomesData) ?? [];
   return (
     <div>
-      {onChange === 'expenses' && entries && (
-        <List>
-          {entries.map(item => {
-            const iconName = item[0].replace(/\s+/g, '');
-
-            return (
-              <Item
-                key={iconName}
-                id={iconName}
-                onClick={clickEventHandler}
-                className={iconName === active ? 'active' : ''}
-              >
-                <p>{item[1].total}.00</p>
-                <ItemSvg width="56" height="56">
-                  <use href={`${reportsIcon}#${iconName}`}></use>
-                </ItemSvg>
-                <p>{categoryOrkToEng(item[0])}</p>
-              </Item>
-            );
-          })}
-        </List>
-      )}
-      {onChange === 'income' && (
-        <List>
-          {incomesEnties.map(item => {
-            const iconName = item[0].replace(/\s+/g, '');
-
-            return (
-              <Item
-                key={iconName}
-                id={iconName}
-                onClick={clickEventHandler}
-                className={iconName === active ? 'active' : ''}
-              >
-                <p>{item[1].total}.00</p>
-                <ItemSvg width="56" height="56">
-                  <use href={`${reportsIcon}#${iconName}`}></use>
-                </ItemSvg>
-                <p>{categoryOrkToEng(item[0])}</p>
-              </Item>
-            );
-          })}
-        </List>
-      )}
+      <List>
+        {entries.map(item => {
+          const iconName = item[0].replace(/\s+/g, '');
+          valueArr.push(item);
+          return (
+            <Item
+              key={iconName}
+              id={iconName}
+              onClick={clickEventHandler}
+              className={iconName === active ? 'active' : ''}
+            >
+              <p>{item[1].total}.00</p>
+              <ItemSvg width="56" height="56">
+                <use href={`${reportsIcon}#${iconName}`}></use>
+              </ItemSvg>
+              <p>{categoryOrkToEng(item[0])}</p>
+            </Item>
+          );
+        })}
+      </List>
     </div>
   );
 };
--- src/redux/reportsQuery/reportsQuery.slice.js
+++ src/redux/reportsQuery/reportsQuery.slice.js
@@ -4,13 +4,18 @@ const reportQuerySlice = createSlice({
   name: 'reportsQuery',
   initialState: {
     reportsQuery: '',
+    filteredData: [],
   },
   reducers: {
     reportsQueryAction(state, action) {
       state.reportsQuery = action.payload;
     },
+    filteredDataAction(state, action) {
+      state.filteredData = action.payload;
+    },
   },
 });
 
-export const { reportsQueryAction } = reportQuerySlice.actions;
+export const { reportsQueryAction, filteredDataAction } =
+  reportQuerySlice.actions;
 export const reportsQuerReducer = reportQuerySlice.reducer;
