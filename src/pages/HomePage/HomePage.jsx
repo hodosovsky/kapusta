c8dd0abe147e2065e341e76a2437a3bf9d503012
maksymomelchuk <omelchukmaksym@gmail.com>
2022-12-25 12:36:33 +0200

    add additional state to home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useMatchMedia } from '../../hooks/use-match-media';
-import GrayBg from "../../components/GrayBg";
+import GrayBg from '../../components/GrayBg';
 import ReportsBtn from '../../components/Buttons/ReportsBtn';
 import ChangeBalance from '../../components/ChangeBalance';
 import DateSelection from '../../components/DateSelection';
 import { StyledHomePage } from './Styles';
 import { TransactionsList } from 'components/TransactionsList/TransactionsList';
-import {TransactionTabsMobile, TransactionTabsDesktop} from "../../components/TransactionTabs/TransactionTabs";
+import {
+  TransactionTabsMobile,
+  TransactionTabsDesktop,
+} from '../../components/TransactionTabs/TransactionTabs';
 
 export default function HomePage() {
   const { isMobile } = useMatchMedia();
-
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <div>
       <GrayBg />
@@ -21,7 +24,9 @@ export default function HomePage() {
           <ChangeBalance />
           {!isMobile && <ReportsBtn to="/reports" />}
         </div>
-        {isMobile && <DateSelection />}
+        {isMobile && (
+          <DateSelection startDate={startDate} setStartDate={setStartDate} />
+        )}
         {isMobile && <TransactionTabsMobile />}
         {!isMobile && <TransactionTabsDesktop />}
         {!isMobile && <Outlet />}
