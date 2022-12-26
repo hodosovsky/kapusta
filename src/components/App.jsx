5a7e8e75b70f91ac154a4879d4d2b6a1af441cc8
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2022-12-26 08:33:37 +0200

    remove container from app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,7 +21,7 @@ import { PublicRoute } from './PublicRoute/PublicRoute';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useMatchMedia } from 'hooks/use-match-media';
-import { Container } from './App.styled';
+
 // import { TransactionsList } from './TransactionsList/TransactionsList';
 // import { selectIsLoadingCurrentUser } from 'redux/selectors';
 
