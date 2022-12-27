b9124243cf3fef9a8bef990286b3c5096d27213e
Oleksandra <sashkaburko1994@gmail.com>
2022-12-28 01:25:46 +0200

    1

--- src/components/Reports/ReportsInfo/List/ReportsList.styled.jsx
+++ src/components/Reports/ReportsInfo/List/ReportsList.styled.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import bgcForSvg from '../../../../images/reportsFiles/bgcForSvg.png';
+import bgcForSvg from '../../../../images/reportsFiles/bgcForSvg.svg';
 
 export const List = styled.ul`
   display: flex;
