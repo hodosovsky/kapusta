d6194301d67b009015a24d34dcfa6de1d3d46cb9
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2022-12-28 09:22:29 +0200

    add google authorization

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { SharedLayouts } from './SharedLayouts/SharedLayouts';
 import { useEffect, lazy } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -24,10 +24,24 @@ const ThereIsNoSuchPage = lazy(() =>
 
 export const App = () => {
   const dispatch = useDispatch();
-
   const token = useSelector(selectToken);
-
   const isFetchingUser = useSelector(selectIsFetcingCurrentUser);
+  const location = window.location;
+  const urlSearchParams = new URLSearchParams(location.search);
+  const accessToken = urlSearchParams.get('accessToken');
+
+  useEffect(() => {
+    if (accessToken) {
+      setAuthHeader(accessToken);
+      dispatch(addAccessToken(accessToken));
+      dispatch(refreshUser());
+      if (location.pathname === '/') {
+        location.pathname = '/kapusta/home';
+        dispatch(refreshUser());
+      }
+    }
+  }, [accessToken, dispatch, location]);
+  //
   const { isMobile } = useMatchMedia();
   useEffect(() => {
     if (!token || token === 'null' || token === null) {
@@ -41,39 +55,41 @@ export const App = () => {
   return (
     !isFetchingUser && (
       <>
-        <ToastContainer />
-        <Routes>
-          <Route path="/" element={<SharedLayouts />}>
-            <Route path="/" element={<PrivateRoute />}>
-              <Route index element={<Navigate to="/home" />} />
-              {!isMobile && (
-                <>
-                  <Route path="/home" element={<HomePage />}>
-                    <Route index element={<Navigate to="/home/expenses" />} />
-                    <Route path="income" element={<IncomePage />} />
-                    <Route path="expenses" element={<ExpensesPage />} />
-                  </Route>
-                </>
-              )}
-              {isMobile && (
-                <>
-                  <Route path="/home" element={<HomePage />} />
-                  <Route path="/income" element={<IncomePage />} />
-                  <Route path="/expenses" element={<ExpensesPage />} />
-                  <Route path="*" element={<Navigate to="/home" />} />
-                </>
-              )}
-              <Route path="/reports" element={<ReportsPage />} />
-            </Route>
-            <Route path="/" element={<PublicRoute />}>
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegiserPage />} />
-              <Route path="*" element={<Navigate to="/login" />} />
-            </Route>
+        <BrowserRouter basename="/kapusta">
+          <ToastContainer />
+          <Routes>
+            <Route path="/" element={<SharedLayouts />}>
+              <Route path="/" element={<PrivateRoute />}>
+                <Route index element={<Navigate to="/home" />} />
+                {!isMobile && (
+                  <>
+                    <Route path="/home" element={<HomePage />}>
+                      <Route index element={<Navigate to="/home/expenses" />} />
+                      <Route path="income" element={<IncomePage />} />
+                      <Route path="expenses" element={<ExpensesPage />} />
+                    </Route>
+                  </>
+                )}
+                {isMobile && (
+                  <>
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/income" element={<IncomePage />} />
+                    <Route path="/expenses" element={<ExpensesPage />} />
+                    <Route path="*" element={<Navigate to="/home" />} />
+                  </>
+                )}
+                <Route path="/reports" element={<ReportsPage />} />
+              </Route>
+              <Route path="/" element={<PublicRoute />}>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegiserPage />} />
+                <Route path="*" element={<Navigate to="/login" />} />
+              </Route>
 
-            <Route path="*" element={<ThereIsNoSuchPage />} />
-          </Route>
-        </Routes>
+              <Route path="*" element={<ThereIsNoSuchPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
         {/* <LightModalWindow>Are you sure?</LightModalWindow> */}
         {/* <Summary /> */}
         {/* <Form /> */}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 import { store, persistor } from 'redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -11,9 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/kapusta">
-          <App />
-        </BrowserRouter>
+        <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>
